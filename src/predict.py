007124e96a13df7ea72ebab8e1a1d848fc4ebca5
varshaasrinivas <varshasriinivas@gmail.com>
2025-07-09 21:00:40 +0530

    Create predict.py

---
+++ src/predict.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import joblib
+
+# Load model
+model = joblib.load('model/model.pkl')
+
+# Sample patient
+sample = pd.DataFrame({
+    'Pregnancies': [10],
+    'Glucose': [98],
+    'BloodPressure': [50],
+    'SkinThickness': [46],
+    'Insulin': [27],
+    'BMI': [30],
+    'Diabetes': [0.672],
+    'Age': [54],
+   
+})
+
+# Predict
+prediction = model.predict(sample)[0]
+risk = "Diabetic" if prediction == 1 else "No Diabetic"
+print(f" Prediction: {prediction} ({Diabetic})")
